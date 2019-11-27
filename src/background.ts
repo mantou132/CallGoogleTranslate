25e9fda1df7163d5c447486868792f679aa33e1c
王先硚 <709922234@qq.com>
2019-11-28 01:55:18 +0800

    add retry

--- src/background.ts
+++ src/background.ts
@@ -1,43 +1,59 @@
-import { browser, Menus } from 'webextension-polyfill-ts';
+import { browser, Menus, Runtime } from 'webextension-polyfill-ts';
 
-// parameter application must match /^\w+(\.\w+)*$/ for runtime.connectNative.
-const port = browser.runtime.connectNative('google_translate_bridge');
+class PortManager {
+  port: Runtime.Port;
 
-function process({ frameId }: Menus.OnClickData) {
-  const code = `(() => {
-    const node = document.activeElement;
-    return node && ['TEXTAREA', 'INPUT'].includes(node.nodeName) ?
-            node.value.slice(node.selectionStart, node.selectionEnd) :
-            window.getSelection().toString();
-  })();`;
+  start = () => {
+    // parameter application must match /^\w+(\.\w+)*$/ for runtime.connectNative.
+    this.port = browser.runtime.connectNative('google_translate_bridge');
 
-  browser.tabs.executeScript({ code, frameId }).then((result = []) => {
-    result[0] &&
-      port.postMessage({
-        type: 'translate-text',
-        data: result[0],
-      });
-  });
+    this.port.onMessage.addListener(({ type }) => {
+      if (type === 'connected') {
+        console.log('connected');
+        this.createMenu();
+      }
+    });
+
+    this.port.onDisconnect.addListener(() => {
+      console.log('disconnect, retry after 10s');
+      this.removeMenu();
+      setTimeout(() => {
+        this.start();
+        console.log('retrying');
+      }, 10_000);
+    });
+  };
+
+  process = ({ frameId }: Menus.OnClickData) => {
+    const code = `(() => {
+      const node = document.activeElement;
+      return node && ['TEXTAREA', 'INPUT'].includes(node.nodeName) ?
+              node.value.slice(node.selectionStart, node.selectionEnd) :
+              window.getSelection().toString();
+    })();`;
+
+    browser.tabs.executeScript({ code, frameId }).then((result = []) => {
+      result[0] &&
+        this.port.postMessage({
+          type: 'translate-text',
+          data: result[0],
+        });
+    });
+  };
+
+  createMenu = () => {
+    browser.contextMenus.create({
+      id: 'googletranslate',
+      title: 'Google Translate',
+      contexts: ['selection'],
+      documentUrlPatterns: ['<all_urls>'], // limiting to supported schemes
+      onclick: this.process,
+    });
+  };
+
+  removeMenu = () => {
+    browser.contextMenus.remove('googletranslate');
+  };
 }
 
-function createMenu() {
-  browser.contextMenus.create({
-    id: 'googletranslate',
-    title: 'Google Translate',
-    contexts: ['selection'],
-    documentUrlPatterns: ['<all_urls>'], // limiting to supported schemes
-    onclick: process,
-  });
-}
-
-function removeMenu() {
-  browser.contextMenus.remove('googletranslate');
-}
-
-port.onMessage.addListener(({ type }) => {
-  if (type === 'connected') {
-    createMenu();
-  }
-});
-
-port.onDisconnect.addListener(removeMenu);
+new PortManager().start();
