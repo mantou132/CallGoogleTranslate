fb56a23cd7273325577eaa6433a5848f65281b56
mantou132 <709922234@qq.com>
2024-06-21 23:53:42 +0800

    feat: use mv3

--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,4 @@
-import { browser, Menus, Runtime } from 'webextension-polyfill-ts';
+import { runtime, scripting, Runtime, contextMenus, tabs } from 'webextension-polyfill';
 
 class PortManager {
   connected = false;
@@ -6,7 +6,7 @@ class PortManager {
 
   start = () => {
     // parameter application must match /^\w+(\.\w+)*$/ for runtime.connectNative.
-    this.port = browser.runtime.connectNative('google_translate_bridge');
+    this.port = runtime.connectNative('google_translate_bridge');
 
     this.port.onMessage.addListener(({ type }) => {
       if (type === 'connected') {
@@ -25,47 +25,61 @@ class PortManager {
     });
   };
 
-  process = ({ frameId }: Menus.OnClickData) => {
-    const code = `(() => {
-      const node = document.activeElement;
-      return node && ['TEXTAREA', 'INPUT'].includes(node.nodeName) ?
-              node.value.slice(node.selectionStart, node.selectionEnd) :
-              window.getSelection().toString();
-    })();`;
-
-    browser.tabs.executeScript({ code, frameId }).then((result = []) => {
-      result[0] &&
-        this.port.postMessage({
-          type: 'translate-text',
-          data: result[0],
-        });
-    });
-  };
-
   createMenu = () => {
     this.connected = true;
-    browser.contextMenus.create({
+    contextMenus.create({
       id: 'googletranslate',
       title: 'Google Translate',
       contexts: ['selection'],
       documentUrlPatterns: ['<all_urls>'], // limiting to supported schemes
-      onclick: this.process,
     });
   };
 
   removeMenu = () => {
     this.connected = false;
-    browser.contextMenus.remove('googletranslate');
+    contextMenus.remove('googletranslate');
   };
 }
 
 const portManager = new PortManager();
 portManager.start();
 
-browser.runtime.onInstalled.addListener(({ reason }) => {
+contextMenus.onClicked.addListener(async function(info) {
+  switch (info.menuItemId) {
+    case 'googletranslate':
+      const [tab] = await tabs.query({ active: true, currentWindow: true });
+      if (!tab?.id) break;
+      scripting
+        .executeScript({
+          target: { frameIds: info.frameId ? [info.frameId] : undefined, tabId: tab.id },
+          func: () => {
+            let node = document.activeElement;
+            while (true) {
+              const shadow = node?.shadowRoot?.activeElement;
+              if (!shadow) break;
+              node = shadow;
+            }
+            console.log(node);
+            return node instanceof HTMLTextAreaElement || node instanceof HTMLInputElement
+              ? node.value.slice(node.selectionStart || 0, node.selectionEnd || -1)
+              : getSelection()?.toString() || '';
+          },
+        })
+        .then((result = []) => {
+          result[0].result &&
+            portManager.port.postMessage({
+              type: 'translate-text',
+              data: result[0].result,
+            });
+        });
+      break;
+  }
+});
+
+runtime.onInstalled.addListener(({ reason }) => {
   setTimeout(() => {
     if (reason === 'install' && !portManager.connected) {
-      browser.tabs.create({ url: browser.runtime.getURL('welcome.html') });
+      tabs.create({ url: runtime.getURL('welcome.html') });
     }
   }, 1_000);
 });
